2b747984361e1cccf92e226bcb65ba9b79a49bef
Nataliia21a <152820534+Nataliia21a@users.noreply.github.com>
2025-03-22 07:36:52 +0200

    add css mainLayoutPage for mobile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,14 +6,16 @@ export default function Header() {
       <svg className={css.logo} width="76" height="20">
         <use href="/svg/symbol-defs.svg#icon-logo-white-mob-main-page"></use>
       </svg>
-      <div className={css.containerSvg}>
-        <svg className={css.user} width="20" height="20">
-          <use href="/svg/symbol-defs.svg#icon-user-02-yellow"></use>
+      <div className={css.userMenu}>
+        <div className={css.containerSvg}>
+          <svg className={css.userSvg} width="20" height="20">
+            <use href="/svg/symbol-defs.svg#icon-user-02-yellow"></use>
+          </svg>
+        </div>
+        <svg className={css.mobMenu} width="40" height="40">
+          <use href="/svg/symbol-defs.svg#icon-mob-menu-white"></use>
         </svg>
       </div>
-      <svg className={css.mobMenu} width="40" height="40">
-        <use href="/svg/symbol-defs.svg#icon-mob-menu-white"></use>
-      </svg>
     </header>
   );
 }
