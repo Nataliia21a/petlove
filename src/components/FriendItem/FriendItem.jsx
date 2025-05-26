766893c7a957fcc0dd0b3d073da01186e5ec6d2f
Nataliia21a <152820534+Nataliia21a@users.noreply.github.com>
2025-05-26 07:06:32 +0300

    add works time and  css style to FriendItem component

--- src/components/FriendItem/FriendItem.jsx
+++ src/components/FriendItem/FriendItem.jsx
@@ -1,35 +1,58 @@
 import css from "./FriendItem.module.css";
 
 export default function FriendItem({ friend }) {
+  const workdays = friend.workDays;
+
+  const firstWorksDay = workdays && workdays.find((day) => day.isOpen === true);
+
   return (
     <div className={css.containerFriends}>
-      <img className={css.img} src={friend.imageUrl} alt={friend.title} />
-      <div>
-        <h2>{friend.title}</h2>
-        <p>
-          Email: <span></span>
-          {friend.email}
-        </p>
-
-        {friend.address ? (
+      <div className={css.workTime}>
+        {firstWorksDay ? (
           <p>
-            Address: <span></span>
-            {friend.address}
+            {firstWorksDay.from} - {firstWorksDay.to}
           </p>
         ) : (
-          <p>Address : website only</p>
+          <p>Day and night</p>
         )}
+      </div>
+      <div className={css.friendDescription}>
+        <img className={css.img} src={friend.imageUrl} alt={friend.title} />
+        <div>
+          <h2 className={css.title}>{friend.title}</h2>
+          <div className={css.contacts}>
+            <p>
+              Email: <span>{friend.email}</span>
+            </p>
 
-        {friend.phone ? (
-          <p>
-            Phone: <span>{friend.phone}</span>
-            {friend.phone}
-          </p>
-        ) : (
-          <p>
-            Phone: <span>email only</span>
-          </p>
-        )}
+            {friend.address ? (
+              <p>
+                <a
+                  href={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
+                    friend.address
+                  )}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Address: <span>{friend.address}</span>
+                </a>
+              </p>
+            ) : (
+              <p>
+                Address : <span>website only</span>
+              </p>
+            )}
+            {friend.phone ? (
+              <p>
+                Phone: <span>{friend.phone}</span>
+              </p>
+            ) : (
+              <p>
+                Phone: <span>email only</span>
+              </p>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
